dd357781b7011d761308b66de1e84604cf01faef
Lee Magnusson <lee@unhuman.io>
2018-11-27 14:27:30 -0800

    simple communication

--- src/messages/periodic_communication.h
+++ src/messages/periodic_communication.h
@@ -1,22 +1,20 @@
 #ifndef MOTOR_PERIODIC_COMMUNICATION_H
 #define MOTOR_PERIODIC_COMMUNICATION_H
 
-#include <mutex>
-
 template <class Command, class Param, class Status>
 class PeriodicCommunication {
 public:
     // TODO figure out if below can be used
     //    PeriodicCommunication() : Command(), Param(), Status() {}
-    void set_command(Command &command) { command_mutex_.lock(); command_in_ = command; command_mutex_.unlock(); }
+    void set_command(Command &command) { command_in_ = command;  }
     // note try_lock below needs to succeed most of the time - maybe need higher frequency sub loops
-    void update_command() { if(command_mutex_.try_lock()) { command_ = command_in_; command_mutex_.unlock(); } }
+    void update_command() {  command_ = command_in_;  }
     // param maybe not worth worrying about
     //void set_param(Param &param) { param_mutex_.lock(); param_in_ = param; param_mutex_.unlock(); }
-    void set_param(Param &param) { param_mutex_.lock(); param_ = param; param_mutex_.unlock(); }
-    void update_param() { param_mutex_.lock(); param_ = param_in_; param_mutex_.unlock(); }
-    void get_status(Status *status) { status_mutex_.lock(); *status = status_out_; status_mutex_.unlock(); }
-    void update_status() { status_mutex_.lock(); status_out_ = status_; status_mutex_.unlock(); }
+    void set_param(Param &param) { param_ = param;  }
+    void update_param() { param_ = param_in_; }
+    void get_status(Status *status) { *status = status_out_; }
+    void update_status() { status_out_ = status_; }
 
     Command command_;
     Param param_;
@@ -25,9 +23,6 @@ private:
     Command command_in_;
     Param param_in_;
     Status status_out_;
-    std::mutex command_mutex_;
-    std::mutex param_mutex_;
-    std::mutex status_mutex_;
 };
 
 
